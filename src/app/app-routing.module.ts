3b8ebe151ce40fc0299fea29f31c44bc12d69303
pepe <andrespaez123@hotmail.com>
2023-08-04 17:03:21 -0500

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListadoMascotasComponent } from './components/listado-mascotas/listado-mascotas.component';
+import { AgregarEditarMascotaComponent } from './components/agregar-editar-mascota/agregar-editar-mascota.component';
+import { VerMascotaComponent } from './components/ver-mascota/ver-mascota.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'listMascotas', pathMatch: 'full'},
+  { path: 'listMascotas', component: ListadoMascotasComponent },
+  { path: 'agregarMascota', component: AgregarEditarMascotaComponent },
+  { path: 'verMascota', component: VerMascotaComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,7 @@ import { AgregarEditarMascotaComponent } from './components/agregar-editar-masco
 import { ListadoMascotasComponent } from './components/listado-mascotas/listado-mascotas.component';
 import { VerMascotaComponent } from './components/ver-mascota/ver-mascota.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-//Angular material
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatTableModule} from '@angular/material/table';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatSortModule} from '@angular/material/sort';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTooltipModule} from '@angular/material/tooltip';
-
+import { SharedModule } from './shared/shared.module';
 
 
 
@@ -36,14 +26,7 @@ import {MatTooltipModule} from '@angular/material/tooltip';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
-    MatTooltipModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
